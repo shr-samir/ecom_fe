559e3d643c4b35eb8a15d8e7d61af4313b3f94da
SujitShres1ha <sthasujit2062@gmail.com>
2025-01-05 21:37:08 -0600

    Add Register page and update Login page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="flex items-center justify-center font-outfit">
       <form className="flex flex-col gap-4 sm:max-w-96 w-[90%] mt-24">
         <div className="flex justify-center items-center gap-2 mb-2">
-          <p className="font-serif text-3xl">Login</p>
+          <p className="font-serif text-3xl">Sign Up</p>
           <hr className="border-none h-[1.75px] w-8 bg-black" />
         </div>
+        <input
+          type="name"
+          placeholder="Name"
+          className="border-[1.5px] border-black px-3 py-2"
+        />
         <input
           type="email"
           placeholder="Email"
@@ -18,16 +23,17 @@ const Login = () => {
           placeholder="Password"
           className="border-[1.5px] border-black px-3 py-2"
         />
-        <p className="flex justify-between">
-          <span className="hover:cursor-pointer">Forgot your password?</span>
-          <Link to={"/register"}>Create account</Link>
+        <p className="flex justify-end">
+          <Link to={"/login"} className="hover:cursor-pointer">
+            Login Here
+          </Link>
         </p>
         <div className="flex justify-center mt-4">
           <button
             type="submit"
             className="bg-black w-fit px-8 py-3 text-white cursor-pointer"
           >
-            Login
+            Sign Up
           </button>
         </div>
       </form>
@@ -35,4 +41,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
